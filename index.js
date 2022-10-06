5ca0afbcc863ca1043b8f506a4508698ff5f5113
seandmatlock <115140728+seandmatlock@users.noreply.github.com>
2022-10-06 09:30:21 +0200

    Added files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let titleEl = document.getElementById("title-text")
+let buttonEl = document.getElementById("message-btn")
+let messageEl = document.getElementById("message")
+
+function message() {
+    titleEl.textContent = "You don't?"
+    buttonEl.textContent = "Hold up, I haven't figured out switch cases yet, so I'll update once I do" 
+}
+
